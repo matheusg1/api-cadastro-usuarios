4d3a72a9c04d83636edef227cae5daaf4851abad
matheus.silva <tecmatheusg@hotmail.com>
2022-10-27 11:29:23 -0300

    Commit inicial

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System.Web.Http;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+
+namespace UsuariosApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto usuarioDto)
+        {
+            //TODO Chamar service
+            return Ok();
+        }
+    }
+}
