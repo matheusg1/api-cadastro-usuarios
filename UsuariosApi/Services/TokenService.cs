527572c9640bac16d8b7f9e404739011e30211c5
matheus.silva <tecmatheusg@hotmail.com>
2022-10-28 13:50:03 -0300

    Criação de tokens implementada

---
+++ UsuariosApi/Services/TokenService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Services
+{
+    public class TokenService
+    {
+        public Token createToken(IdentityUser<int> usuario)
+        {
+            Claim[] direitosUsuario = new Claim[]
+            {
+                new Claim("username", usuario.UserName),
+                new Claim("id", usuario.Id.ToString())
+            };
+
+            var chave = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("asd54dd89d89d8e7t8e4t11qik5cai")
+                );
+            var credenciais = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: direitosUsuario,
+                signingCredentials: credenciais,
+                expires: DateTime.UtcNow.AddHours(1)
+                );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return new Token(tokenString);
+        }
+    }
+}
